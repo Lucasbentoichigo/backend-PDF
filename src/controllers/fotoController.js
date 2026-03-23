6f63824385bff11e78acf5725f887a816961515b
lucas <lucastavaresbento@gmail.com>
2026-03-23 15:54:42 -0300

    fix: resolução de erros no fotoHelper e fotoController

--- src/controllers/fotoController.js
+++ src/controllers/fotoController.js
@@ -1,6 +1,6 @@
 import AlunoModel from '../models/AlunoModel.js';
 import fs from 'fs';
-import { upload, removerFoto } from '../utils/fotoHelper.js';
+import { processarFoto, removerFoto } from '../utils/fotoHelper.js';
 
 export const uploadFoto = async (req, res) => {
     try {
